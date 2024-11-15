f05eeb149c493f7e5517ea1db303c4cb94c83e5a
Luis Ballado <alberto.balladoar@gmail.com>
2024-11-14 23:07:19 -0600

    prob3_bfs

--- problema2/dfs.cpp
+++ problema2/dfs.cpp
@@ -1,9 +1,5 @@
 #include <iostream>
-#include <queue>
 #include <vector>
-#include <stack>
-#include <list>
-#include <set>
 
 //Empresa de transportes
 //Conocer:
@@ -25,7 +21,7 @@ par de almacenes ya que esto le permitirá tomar decisiones estratégicas para
 hacer más eficiente la operación de su empresa. Dado este problema práctico, 
 desarrolle los siguientes puntos:
 a) Diseñe e implemente en C++ un algoritmo recursivo basado en DFS para encontrar
-la ruta más larga entre cualquier par de almacenes de la empresa. 
+la ruta más larga entre cualquier par de almacenes de la empresa.
 El programa recibe por redirección de la lı́nea de comandos 
 el nombre de un archivo de texto que contiene un grafo ponderado 
 dirigido con el formato que se utilizó en clase (sesión 9). 
@@ -37,34 +33,92 @@ Asegurese de utilizar una lista de adyacencia para representar el grafo.
 
  **/
 
+//variables globales para tener acceso dentro de las funciones
 typedef std::pair<int,int> arista;
 
 int predfn = 0;
 int postdfn = 0;
-std::vector<bool> visitados;
-std::vector<std::vector<arista>> adj_list;
 
-//DFS
-//int dfs(std::vector<std::vector<arista>> &adj_list){
-int dfs(int v){
+std::vector<bool> visitados;               //visitados
+std::vector<std::vector<arista>> adj_list; //lista de adjacencia
 
-  predfn = predfn + 1;
+//int costo;
+
+std::vector<int> arr;
+std::vector<std::pair<int,int>> combinaciones;
+
+void combinationUtil(std::vector<int> arr,int data[],int start,int end,int index,int r);
+
+int costo = 0;
+
+//encontrar todos los caminos
+std::vector<std::vector<int>> caminos(int inicio, int fin){
+  std::vector<std::vector<int>> lista_caminos;
+  std::vector<int> ruta_actual;
+  std::vector<bool> visitados;
+  dfs(inicio,fin,ruta_actual,visitados,lista_caminos);
+  return lista_caminos;
+}
+
+//DFS recursivo
+int dfs(int v,int fin,std::vector<int> ruta_actual,std::vector<bool> visitados, std::vector<std::vector<int>> lista_caminos){
+
+  ruta_actual.push_back(v);
+  
   visitados[v] = true;
-
+  
   for(int i=0;i<adj_list[v].size();i++){
-    std::cout << "Analizando nodo: " << v << std::endl;
-    std::cout << "visite: " << adj_list[v][i].first
-	      << " w: " << adj_list[v][i].second <<std::endl;
+
     if(visitados[adj_list[v][i].first]==false){
-      if(visitados[adj_list[v][i].first]==false){
-	dfs(adj_list[v][i].first);
+
+      std::cout << "Analizando nodo: " << v << "a visitar: " << adj_list[v][i].first
+		<< " w: " << adj_list[v][i].second <<std::endl;
+
+      costo = costo + adj_list[v][i].second; //si no esta visitado sumarlo al costo
+      
+      
+      if(adj_list[v][i].first == fin){ //si es el resultado parar
+	std::cout << "encontre el camino costo::" << costo<< std::endl;
+	//std::cout << "encontre el camino ::" << std::endl;
+	costo = 0;
+	return 0;
       }
+
+      
+      //peso = dfs(adj_list[v][i].first);      
+
+      
+      //************cosas del problema*******************
+
+      dfs(adj_list[v][i].first,fin);
+      
+      
     }
   }
 
-  postdfn = postdfn + 1;
+  //postdfn = postdfn + 1;
+  //std::cout << "post: " << postdfn << std::endl;
+  return -1;
+}
+
+//https://www.geeksforgeeks.org/print-all-possible-combinations-of-r-elements-in-a-given-array-of-size-n/
+void combination(std::vector<int> arr, int n, int r){
+  int data[r];
+  combinationUtil(arr,data,0,n-1,0,r);
+}
+
+void combinationUtil(std::vector<int> arr,int data[],int start,int end,int index,int r){
+  if(index ==r){
+    //std::cout << data[0] << "," << data[1] << std::endl;
+    combinaciones.push_back(std::make_pair(data[0],data[1]));
+    return;
+  }
+
+  for(int i=start;i<=end&&end-i+1>=r-index;i++){
+    data[index] = arr[i];
+    combinationUtil(arr,data,i+1,end,index+1,r);
+  }
   
-  return 0;
 }
 
 int main(){
@@ -72,43 +126,85 @@ int main(){
   int num_ciudades;
   int num_carreteras;
   
+  int peso;
+
   //------ construir adj list ------------
   std::cin >> num_ciudades >> num_carreteras;
-
+  
+  //TODO: fix cero basado uno basado
   adj_list.resize(num_ciudades+1);
   visitados.resize(num_ciudades+1);
   
   for(int i=0;i<num_carreteras;i++){
     
-    int u,v,peso;
-    std::cin >> u >> v >> peso;
-    //TODO: fix cero basado uno basado
-    adj_list[u].push_back(arista(v,peso));
+    int u,v,w;
+    std::cin >> u >> v >> w;
+    adj_list[u].push_back(arista(v,w));
     
   }
   //--------------------------------------
+
   
   predfn  = 0;
   postdfn = 0;
   
   for(int i=0;i<adj_list.size();i++){
     visitados[i] = false;
-    //std::cout << "*****" << std::endl;
-    //std::cout << "Nodo:: " << i+1 << std::endl;
-    //std::cout << "-----" << std::endl;
-    //for(int j=0;j<adj_list[i].size();j++){
-    //  std::cout << i+1 << " - " << adj_list[i][j].first << " - w:" << adj_list[i][j].second << std::endl;
-    //}
+    arr.push_back(i);
   }
 
+  /*
+    for(int i=0;i<adj_list.size();i++){
+    if(visitados[i]==false){
+      dfs(i);
+    }
+  }
+  */
+
+  int r = 2;
+  int n = adj_list.size();
+
+  combination(arr,n,r);
+
+  //iterar en los pares
+  //for(int i=0;i<combinaciones.size();i++){
+    
+  //std::cout << combinaciones[i].first <<","<< combinaciones[i].second<< std::endl;
+
+  int inicio = 1;//combinaciones[i].first;
+  int fin = 8;//combinaciones[i].second;
+
+  if(visitados[inicio]==false){
+    dfs(inicio,fin);
+  }
+  //  std::vector<bool> visitados(n,false);
+  //}
+  
+  /*
+  for(int i=0;i<combinaciones.size();i++){
+
+    //std::cout << combinaciones[i].first <<","<< combinaciones[i].second<< std::endl;
+
+    std::cout << "VISITAR:" << combinaciones[i].first << "," << combinaciones[i].second << std::endl;
+
+    
+
+    
+    
+  }
+  */
+    
+
+  /*
   for(int i=0;i<adj_list.size();i++){
     if(visitados[i]==false){
       dfs(i);
     }
   }
+  */
+
   
-  //
-  std::cout << predfn << std::endl;
-  std::cout << postdfn << std::endl;
+  
   return 0;
+
 }
--- problema3_bfs/bfs.cpp
+++ problema3_bfs/bfs.cpp
@@ -1,48 +1,88 @@
-#include <iostream> //entrada de datos
-#include <fstream>  //escribir a archivo
-#include <chrono>   //medir tiempo
+#include <iostream>
+#include <chrono>
+#include <vector>
+#include <queue>
 
 //Un grafo es bipartita si todos sus vértices pueden ser divididos en dos subconjuntos disjuntos X y Y de forma tal que cada arco conecte un vertice en X con uno en Y.
+//referencia https://www.geeksforgeeks.org/bipartite-graph/#using-breadthfirst-search-bfs
 
 //Diseñe un algoritmo basado en BFS para verificar si un grafo es bipartita o no.
-//5 4
-//0 1
-//1 2
-//1 4
-//2 3
+
+bool esBipartito(int V, std::vector<std::vector<int>> adj_list){
+  int n = V;
+  std::vector<int> visitado(n,-1);
+  
+  for(int i=0;i<n;i++){
+    if(visitado[i]==-1){
+
+      //BFS
+      std::queue<int> cola;
+      visitado[i] = 0;
+      cola.push(i);
+      
+      while(!cola.empty()){
+	int nodoActual = cola.front();
+	cola.pop();
+	
+	for(auto &v : adj_list[nodoActual]){
+	  if(visitado[v]==-1){
+	    visitado[v] = 1 - visitado[nodoActual];
+	    cola.push(v);
+	  }else if(visitado[v]==visitado[nodoActual]){
+	    return false;
+	  }
+	  
+	}
+      }
+      //BFS
+    }
+  }
+
+  return true;
+}
 
 int main(){
 
-  //***C++11 Style:***
+  //medir tiempo
   std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
   
-  int dato;
+  int vertice; 
+  int arista;
 
-  std::cin >> dato;
-    
+  //guardar datos
+  std::cin >> vertice >> arista;
+
+  //se crea un array de vectores con la cardinalidad de vertices(nodos)
+  std::vector<std::vector<int>> adj(vertice+1);
   
-  //escribir el dato en archivo de salida
-  std::ofstream output_file("archivo_salida.txt");
+  //ir poblando con la lectura de consola respecto
+  //al num de aristas, este indica el num de conexiones en el grafo
+  //decimos que existe una conexion entre el nodo 0 y nodo 1 de la siguiente forma: 0 1
+  for (int i = 0; i < arista; i++) {
 
-  if(output_file.is_open()){
-    //iterar en una lista
-    //for(const auto& pair: lista){
-      
-    //}
-
-    output_file << "El dato es: " << dato << std::endl;
-    
-  }else{
-    std::cerr << "Errores " << std::endl;
+    int v1, v2;  //auxiliares que representan los vertices
+    std::cin >> v1 >> v2; 
+    //std::cout << v1 << "-" << v2 << std::endl;
+    adj[v1].push_back(v2); //agregar en dos vias las 
+    adj[v2].push_back(v1); //conexiones del grafo
   }
+  
 
+  //imprimir resultados
+  
+  if(esBipartito(vertice,adj)==true){
+    std::cout << "El grafo SI es bipartita" << std::endl;
+  }else{
+    std::cout << "El grafo NO es bipartita" << std::endl;
+  }
+  
   //-------------------------------------------------------------------------------
   std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 
-  std::cout << "Tiempo = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[µs]" << std::endl;
-  std::cout << "Tiempo = " << std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() << "[ns]" << std::endl;
-  std::cout << "Tiempo = " << std::chrono::duration_cast<std::chrono::milliseconds> (end - begin).count() << "[ms]" << std::endl;
-  std::cout << "Tiempo = " << std::chrono::duration_cast<std::chrono::seconds> (end - begin).count() << "[s]" << std::endl;
+  std::cout << " - tiempo = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[µs]" << std::endl;
+  std::cout << " - tiempo = " << std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() << "[ns]" << std::endl;
+  std::cout << " - tiempo = " << std::chrono::duration_cast<std::chrono::milliseconds> (end - begin).count() << "[ms]" << std::endl;
+  std::cout << " - tiempo = " << std::chrono::duration_cast<std::chrono::seconds> (end - begin).count() << "[s]" << std::endl;
   
   return 0;
 }
